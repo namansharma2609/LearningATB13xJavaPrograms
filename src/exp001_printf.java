63987c56c11828ccf5bf06b6f5f8206da446adbb
Naman Sharma <sharmanaman26099@gmail.com>
2025-06-11 00:34:12 +0530

    git commit

---
+++ src/exp001_printf.java
@@ -0,0 +1,16 @@
+public class exp001_printf {
+    public static void main(String[] args) {
+        int n=9;
+        System.out.println("Table of 9");
+        System.out.printf("%d X %d = %d%n",n,1,n);
+        System.out.printf("%d X %d = %d%n",n,2,n*2);
+        System.out.printf("%d X %d = %d%n",n,3,n*3);
+        System.out.printf("%d X %d = %d%n",n,4,n*4);
+        System.out.printf("%d X %d = %d%n",n,5,n*5);
+        System.out.printf("%d X %d = %d%n",n,6,n*6);
+        System.out.printf("%d X %d = %d%n",n,7,n*7);
+        System.out.printf("%d X %d = %d%n",n,8,n*8);
+        System.out.printf("%d X %d = %d%n",n,9,n*9);
+        System.out.printf("%d X %d = %d%n",n,10,n*10);
+    }
+}
