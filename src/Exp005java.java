6f6e6e9ddf02fbb1a64446afcedd681932469255
Naman Sharma <sharmanaman26099@gmail.com>
2025-06-10 16:42:21 +0530

    git commit

---
+++ src/Exp005java.java
@@ -0,0 +1,9 @@
+public class Exp005java {
+    public static void main(String[] args) {
+        int N=9;
+        System.out.println("Multiplication Table for "+ N);
+        for(int i=1;i<11;i++) {
+            System.out.println(N + "*" + i + "="+ N*i);
+        }
+    }
+}
