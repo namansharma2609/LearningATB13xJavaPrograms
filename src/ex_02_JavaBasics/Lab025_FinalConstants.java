90a1c5cfd715be53ebd8e89760aed510590231e6
Naman Sharma <sharmanaman26099@gmail.com>
2025-07-05 15:44:30 +0530

    03 July 2025 - Task

--- src/ex_02_JavaBasics/Lab025_FinalConstants.java
+++ src/ex_02_JavaBasics/Lab025_FinalConstants.java
@@ -2,12 +2,8 @@ package ex_02_JavaBasics;
 
 public class Lab025_FinalConstants {
     public static void main(String[] args) {
-        final int aa = 112;
-//        aa =100;
-        System.out.println(aa);
-
-        final float PI = 3.14f;
-        System.out.println(PI);
+        String s = "";
+        s.isEmpty();
 
     }
 }
